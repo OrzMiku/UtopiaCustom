5d05a1de1abff82d6f0db16c88adc295b35341ec
OrzMiku <miku@ecy.pink>
2025-08-15 23:50:09 +0800

    feat: 新增2元、500元和1000元面额

--- src/main/java/show/miku/item/UtopiaCustomItems.java
+++ src/main/java/show/miku/item/UtopiaCustomItems.java
@@ -17,18 +17,20 @@ import show.miku.UtopiaCustom;
 public class UtopiaCustomItems {
     public static final RegistryKey<ItemGroup> UTOPIA_CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(UtopiaCustom.MOD_ID, "item_group"));
     public static final ItemGroup UTOPIA_CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
-        .icon(() -> UtopiaCustomItems.LOTTERY_TICKET.getDefaultStack())
+        .icon(UtopiaCustomItems.LOTTERY_TICKET::getDefaultStack)
         .displayName(Text.translatable("itemGroup." + UtopiaCustom.MOD_ID))
         .build();
 
     public static final Item LOTTERY_TICKET = register("lottery_ticket", new Item(new FabricItemSettings()));
     public static final Item MONEY = register("money", new Item(new FabricItemSettings()));
+    public static final Item MONEY_2 = register("money_2", new Item(new FabricItemSettings()));
     public static final Item MONEY_5 = register("money_5", new Item(new FabricItemSettings()));
     public static final Item MONEY_10 = register("money_10", new Item(new FabricItemSettings()));
     public static final Item MONEY_20 = register("money_20", new Item(new FabricItemSettings()));
     public static final Item MONEY_50 = register("money_50", new Item(new FabricItemSettings()));
     public static final Item MONEY_100 = register("money_100", new Item(new FabricItemSettings()));
-
+    public static final Item MONEY_500 = register("money_500", new Item(new FabricItemSettings()));
+    public static final Item MONEY_1000 = register("money_1000", new Item(new FabricItemSettings()));
     public static <T extends Item> T register(String id, T item) {
         Identifier identifier = Identifier.of(UtopiaCustom.MOD_ID, id);
         Registry.register(Registries.ITEM, identifier, item);
@@ -39,11 +41,14 @@ public class UtopiaCustomItems {
     public static void registerFuel() {
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.LOTTERY_TICKET, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY, 20);
+        FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_2, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_5, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_10, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_20, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_50, 20);
         FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_100, 20);
+        FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_500, 20);
+        FuelRegistry.INSTANCE.add(UtopiaCustomItems.MONEY_1000, 20);
     }
 
     // Register items
@@ -61,11 +66,14 @@ public class UtopiaCustomItems {
         ItemGroupEvents.modifyEntriesEvent(UTOPIA_CUSTOM_ITEM_GROUP_KEY).register(entries -> {
             entries.add(UtopiaCustomItems.LOTTERY_TICKET);
             entries.add(UtopiaCustomItems.MONEY);
+            entries.add(UtopiaCustomItems.MONEY_2);
             entries.add(UtopiaCustomItems.MONEY_5);
             entries.add(UtopiaCustomItems.MONEY_10);
             entries.add(UtopiaCustomItems.MONEY_20);
             entries.add(UtopiaCustomItems.MONEY_50);
             entries.add(UtopiaCustomItems.MONEY_100);
+            entries.add(UtopiaCustomItems.MONEY_500);
+            entries.add(UtopiaCustomItems.MONEY_1000);
         });
     }
 
